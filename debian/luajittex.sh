ac20b6b15d840b3ffe381c1b0a4d95f25d987043
Norbert Preining <norbert@preining.info>
2014-05-30 13:01:40 +0900

    add initial version of luajittex script for not supported archs

---
+++ debian/luajittex.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# we need to parse the comman dline arguments,
+# pull out
+#	-jobname=<foobar>
+# and touch
+#	<foobar>.fmt
+#	<foobar>.log
+
+echo "luajittex is not supported on this architecture!" >&2
+
+exit 0
+
